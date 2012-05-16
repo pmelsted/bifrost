c57a4925ed56831c820a8211b8f94d4e5aa4bf73
Trausti Sæmundsson <trauzti@gmail.com>
2012-05-16 15:05:03 +0000

    Now KmerTest also checks backward and forwards bases

--- tests/KmerTest.cpp
+++ tests/KmerTest.cpp
@@ -60,36 +60,60 @@ int main(int argc, char *argv[]) {
 
     // Read k from argument and set kmer size
     unsigned int k = atoi(argv[1]), index = 0, limit = (int) pow(4, k);
+    int j;
     Kmer::set_k(k);
 
     // Allocate strings to use
     char *real = (char *) malloc((k + 1) * sizeof(char));
     char *fromkmer = (char *) malloc((k + 1) * sizeof(char));
     char *realtwin = (char *) malloc((k + 1) * sizeof(char));
+    char letters[] = {'A', 'C', 'G', 'T'};
     for(index=0; index <k; index++)
         real[index] = 'A';
     real[k] = '\0';
     index = 0;
+    Kmer K, TWIN, FW, BACK;
     while (index < limit) {
         makeKmerString(real, index++);
 
-        Kmer KM(real);
+        K = Kmer(real);
 
         // Check the kmer
-        KM.toString(fromkmer);
+        K.toString(fromkmer);
         if (strcmp(real, fromkmer) != 0) {
             cout << "Was expecting the base string to be: " << real << " but got: " << fromkmer << endl;
             return 0;
         }
 
         // Check the twin kmer
-        Kmer TW = KM.twin();
-        TW.toString(fromkmer);
+        TWIN = K.twin();
+        TWIN.toString(fromkmer);
         twinString(real, realtwin, k);
         if (strcmp(fromkmer, realtwin) != 0) {
             cout << "Was expecting twin to be: " << realtwin << " but got: " << fromkmer << endl;
             cout << "The base string was: " << real << endl;
             return 0;
+        }
+        
+        for(j=0; j<4; j++) { 
+            // Check the forward base
+            FW = K.forwardBase(letters[j]);
+            FW.toString(fromkmer);
+            if ((strncmp(&real[1], fromkmer, k-1) != 0) || (letters[j] != fromkmer[k-1])) {
+                printf("Was expecting the forward base to be: %s%c  but got: %s \n", &real[1], letters[j], fromkmer);
+                return 0;
+            }
+        }
+        
+        for(j=0; j<4; j++) { 
+            // Check the backward base
+            BACK = K.backwardBase(letters[j]);
+            BACK.toString(fromkmer);
+            if ((strncmp(real, &fromkmer[1], k-1) != 0) || (letters[j] != fromkmer[0])) {
+                real[k-1] = '\0';
+                printf("Was expecting the backward base to be: %c%s  but got: %s \n", letters[j], real, fromkmer);
+                return 0;
+            }
         }
     }
 
