4c54b1ffda34c858c5496272ba3e0dc6ec13669d
pmelsted <pmelsted@gmail.com>
2012-05-21 15:53:00 +0000

    workflow for blocked bloom filter test

--- tests/BlockedBloomFilterTest.cpp
+++ tests/BlockedBloomFilterTest.cpp
@@ -5,8 +5,18 @@
 
 int main(void) {
 	//BF.insert();
+  // take bits as parameter
 	BlockedBloomFilter BF(10, (size_t) 4, (uint32_t) time(NULL));
 	BF.insert(30);
 	for (int j=0; j<100;j++)
 		cout << BF.contains(j) <<  endl;
+
+
+	// insert ints 0,...,1M
+
+	// assert 0,...,1M
+
+	// check 1M+1,...,10M
+
+	// compute false positive rate
 }
