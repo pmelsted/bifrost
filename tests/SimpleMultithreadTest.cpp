fc9a71bffbe0d2f7591d511f99f675c69a797e6c
kmer <kmer@130.208.71.78>
2012-06-26 10:33:59 +0000

    Using the array of vectors method instead

--- tests/SimpleMultithreadTest.cpp
+++ tests/SimpleMultithreadTest.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <stdlib.h>
 #include <vector>
+#include <omp.h>
 
 using namespace std;
 
@@ -9,38 +10,47 @@ int main () {
   int MAX = 100, reads = 0;
   int l[MAX];
   int sum = 0;
-  vector<int> v, vi;
+  vector<int> *p;
+  vector<int> *v;
+  size_t threadcount = 0;
 
-  while (reads < MAX && cin >> l[reads++]);
+  while (reads < MAX && cin >> l[reads]) {
+    ++reads;
+  }
 
-  #pragma omp parallel default(shared) private(vi)
+  #pragma omp parallel
   {
-    #pragma omp for nowait
-    for (int i=0; i<reads; i++) {
-      sum += l[i];
-      vi.push_back(l[i]);
-    }
-
     #pragma omp master 
     {
-      cout << "Number of threads : " << omp_get_num_threads() << endl;
+      threadcount = omp_get_num_threads();
+      cout << "Number of threads : " << threadcount << endl;
     }
+  }
+  v = new vector<int>[threadcount];
 
-    #pragma omp critical 
-    { 
-      v.insert(v.end(), vi.begin(), vi.end()); 
+  #pragma omp parallel default(shared) private(p)
+  {
+    p = &v[omp_get_thread_num()];
+    #pragma omp for nowait
+    for (int i=0; i<reads; i++) {
+      printf("l[%d]=%d\n", i, l[i]);
+      sum += l[i];
+      p->push_back(l[i]);
     }
   }  
 
 
   #pragma omp parallel shared(sum)
   {
-    cout << "I'm a thread!" << endl;
+    cout << "I'm a thread!\n";
   }
 
   cout << "Sum: " << sum << endl;
-
-  for(vector<int>::iterator it=v.begin(); it != v.end(); ++it) {
-    printf("from vector: %d\n", *it);
+  for (int i=0; i<threadcount; i++) {
+    for(vector<int>::iterator it=v[i].begin(); it != v[i].end(); ++it) {
+      printf("from vector: %d\n", *it);
+    }
   }
+
+  delete[] v;
 }
