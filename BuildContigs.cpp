a7de5cd64eb9a398a42a69a99ba9964faffa0f76
kmer <kmer@130.208.71.78>
2012-07-17 13:01:27 +0000

    fixed a bug

--- BuildContigs.cpp
+++ BuildContigs.cpp
@@ -464,10 +464,10 @@ void BuildContigs_Normal(const BuildContigs_ProgramOptions &opt) {
     }
   }
 
+  pair<pair<size_t, size_t>, size_t> contigDiff = mapper.splitAndJoinContigs();
   // Print the good contigs
   if (opt.verbose) {
     size_t contigsBefore = mapper.contigCount();
-    pair<pair<size_t, size_t>, size_t> contigDiff = mapper.splitAndJoinContigs();
     int contigsAfter = contigsBefore + contigDiff.first.first - contigDiff.first.second - contigDiff.second;
     cerr << "Before split and join: " << contigsBefore << " contigs" << endl;
     cerr << "After split and join: " << contigsAfter << " contigs" <<  endl;
