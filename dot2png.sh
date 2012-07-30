09071398e0f0ccec624157b969409252999a6fa4
Trausti Sæmundsson <trauzti@gmail.com>
2012-07-30 10:21:39 +0000

    Fixed dot2png

--- dot2png.sh
+++ dot2png.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [ -e "$1.dot" ]; then
-    dot -Tps "$1.dot" -o "$1.png"
+    dot -Tpng "$1.dot" -o "$1.png"
     echo "Created $1.png"
 else
     echo -e "usage: ./dot2png <prefix>\n  and <prefix>.dot will be converted to <prefix>.png"
