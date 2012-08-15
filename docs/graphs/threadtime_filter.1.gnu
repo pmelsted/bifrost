e42babc3fd1c149fcedc63bf8c508741a03a5030
Trausti Sæmundsson <trausti@greenqloud.com>
2012-08-15 15:50:42 +0000

    Graphs of time vs threads

---
+++ docs/graphs/threadtime_filter.1.gnu
@@ -0,0 +1,7 @@
+set terminal png
+set output 'threadtime_filter.1.png'
+set grid nopolar
+set xlabel 'threads'
+set ylabel 'time [s]'
+set title "Running time of FilterReads as a function of threads (average of 10 runs)" 
+plot 'threadtime_filter.1.dat' using 1:2 notitle with lines
--- example.sh
+++ example.sh
@@ -8,15 +8,8 @@ then
     exit 1
 fi
 
+#1) Filter the reads with kmersize 31y (this runs in one thread since parameter -t is not given)
+./BFGraph filter example/tinyread_*.fq -k 31 -o example/output/tiny.bf -n 8000 -N 4000 -v
 
-#1) Filter the reads in 1 thread with kmersize 31
-./BFGraph filter example/tinyread_*.fq -k 31 -t 1 -o example/output/tiny.bf -n 8000 -N 4000 -v
-
-#2) Make the contigs, kmersize must be the same as in #1
-./BFGraph contigs example/tinyread_*.fq -k 31 -t 1 -f example/output/tiny.bf -o example/output/tiny -v
-
-#3) Create the graph
-./make_graph.py example/output/tiny
-
-#4) Tell the user how to create a PNG from the .dot file
-echo -e "\nYou can now run: \`./dot2png.sh example/output/tiny\`\nto create 'example/output/tiny.png', a PNG of the De Brujin graph"
+#2) Make the contigs, kmersize must be the same as in #1 (this runs in one thread since parameter -t is not given)
+./BFGraph contigs example/tinyread_*.fq -k 31 -f example/output/tiny.bf -o example/output/tiny -v
